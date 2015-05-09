b21018d1fe4819ef717550601d32e3546c5cd298
Robert James <rjames93@sucs.org>
2015-05-09 13:20:16 +0100

    Changes to be committed: 	modified:   potts.cpp
    Removed some update algorithm logic that 'should' have never been called after the last few commits but just to be certain it has been removed

--- potts.cpp
+++ potts.cpp
@@ -15,96 +15,84 @@
 
 // Class Initialisation, goes though and assigns values
 POTTS_MODEL::POTTS_MODEL(){
-  generator.seed(std::chrono::system_clock::now().time_since_epoch().count());
+	generator.seed(std::chrono::system_clock::now().time_since_epoch().count());
 }
 
 POTTS_MODEL::~POTTS_MODEL(){
-    if(wanglandau == true){
-        // Wang Landau Cleanup
-        for(unsigned int i = 0; i < size; i++){
-            delete [] grid[i];
-        }
-        delete [] grid;
-        delete [] estar;
-        delete [] aguess;
-    } else {
-        // Metropolis Cleanup
-        for(unsigned int i = 0; i < size; i++){
-            delete [] grid[i];
-        }
-        delete [] grid;
-        // Delete Measurements
-        delete [] energy;
-        delete [] magnetisation;
-    }
+	if(wanglandau == true){
+		// Wang Landau Cleanup
+		for(unsigned int i = 0; i < size; i++){
+			delete [] grid[i];
+		}
+		delete [] grid;
+		delete [] estar;
+		delete [] aguess;
+	} else {
+		// Metropolis Cleanup
+		for(unsigned int i = 0; i < size; i++){
+			delete [] grid[i];
+		}
+		delete [] grid;
+		// Delete Measurements
+		delete [] energy;
+		delete [] magnetisation;
+	}
 
 }
 
 double POTTS_MODEL::energycalc(){
-    double energy = 0.0;
+	double energy = 0.0;
 
-    for(unsigned int j = 0; j < size; j++){
-      for(unsigned int i = 0; i < size; i++){
-          if(interface == true && i == interfacepoint){
-              if(grid[i][j] == (grid[(i+1)%size][j]+k)%n_q){
-                  energy++;
-              }
-          } else {
-              if(grid[i][j] == (grid[(i+1)%size][j])){
-                  energy++;
-              }
-          }
+	for(unsigned int j = 0; j < size; j++){
+		for(unsigned int i = 0; i < size; i++){
+			if(grid[i][j] == (grid[(i+1)%size][j])){
+				energy++;
+			}
+			// For neighbour below
 
-          // For neighbour below
+			if(grid[i][j] == grid[i][(j+1)%size]){
+				energy++;
+			}
 
-	  if(grid[i][j] == grid[i][(j+1)%size]){
-		  energy++;
-	  }
-
-      }
-    }
-    //energy *= -1 * ((n_q - 1) / (n_q));
-    energy *= -1;
-    return(energy);
+		}
+	}
+	//energy *= -1 * ((n_q - 1) / (n_q));
+	energy *= -1;
+	return(energy);
 }
 
 double POTTS_MODEL::energychange(unsigned int i, unsigned int j){
-    double energy = 0.0;
+	double energy = 0.0;
 
-    if(interface == true && i == interfacepoint){
-	    if(grid[i][j] == (grid[(i+1)%size][j]+k)%n_q){
-		    energy++;
-	    }
-    } else {
-	    if(grid[i][j] == grid[(i+1)%size][j]){
-	        energy++;
-	    }
-    }
-    if(grid[i][j] == grid[i][(j+1)%size]){
-        energy++;
-    }
-    if(grid[i][j] == grid[i][(j-1)%size]){
-        energy++;
-    }
+	if(grid[i][j] == grid[(i+1)%size][j]){
+		energy++;
+	}
 
-    if(grid[i][j] == grid[(i-1)%size][j]){
-        energy++;
-    }
+	if(grid[i][j] == grid[i][(j+1)%size]){
+		energy++;
+	}
+	if(grid[i][j] == grid[i][(j-1)%size]){
+		energy++;
+	}
 
-    energy *= -1;
-    return(energy);
+	if(grid[i][j] == grid[(i-1)%size][j]){
+		energy++;
+	}
+
+	energy *= -1;
+	return(energy);
 }
 
 double POTTS_MODEL::magnetisationcalc(){
-    double magnetisation = 0.0;
-    double real = 0.0;
-    double imag = 0.0;
-    for(unsigned int j = 0; j < size; j++){
-        for(unsigned int i = 0; i < size; i++){
-	        real += cos(angles[grid[i][j]]);
-	        imag += sin(angles[grid[i][j]]);
-        }
-    }
-    magnetisation = sqrt( (real * real) + (imag * imag) );
-    return(magnetisation);
+	double magnetisation = 0.0;
+	double real = 0.0;
+	double imag = 0.0;
+	for(unsigned int j = 0; j < size; j++){
+		for(unsigned int i = 0; i < size; i++){
+			real += cos(angles[grid[i][j]]);
+			imag += sin(angles[grid[i][j]]);
+		}
+	}
+	magnetisation = sqrt( (real * real) + (imag * imag) );
+	return(magnetisation);
 }
